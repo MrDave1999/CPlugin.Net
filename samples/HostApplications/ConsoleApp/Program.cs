981bdfb70f19e46d8b022f1641be477ff0b6028a
MrDave1999 <daveseva2010@hotmail.es>
2023-11-14 12:40:36 -0500

    docs: Add host applications as samples

---
+++ samples/HostApplications/ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+new EnvLoader().Load();
+Console.WriteLine("PLUGINS=" + EnvReader.Instance["PLUGINS"]);
+Console.WriteLine();
+
+var envConfiguration = new CPluginEnvConfiguration();
+PluginLoader.SetConfiguration(envConfiguration);
+var contracts = PluginLoader.Load<ICommand>();
+foreach(ICommand contract in contracts)
+{
+    contract.Execute();
+}
