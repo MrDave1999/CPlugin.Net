2984e5df8fb5b0ab3f5a1be8731be5264471d0aa
MrDave1999 <daveseva2010@hotmail.es>
2023-11-14 12:37:32 -0500

    docs: Add sample plugins

---
+++ samples/Plugins/OldJsonPlugin/CreateJson.cs
@@ -0,0 +1,31 @@
+﻿[assembly: Plugin(typeof(CreateJson))]
+
+namespace OldJsonPlugin;
+
+public class CreateJson : ICommand
+{
+    public string Name => "oldjson";
+    public string Description => "Outputs JSON value.";
+
+    private class Info
+    {
+        public string JsonVersion;
+        public string JsonLocation;
+        public string Machine;
+        public DateTime Date;
+    }
+
+    public int Execute()
+    {
+        Assembly jsonAssembly = typeof(JsonConvert).Assembly;
+        var info = new Info
+        {
+            JsonVersion  = jsonAssembly.FullName,
+            JsonLocation = jsonAssembly.Location,
+            Machine      = Environment.MachineName,
+            Date         = DateTime.Now
+        };
+        Console.WriteLine(JsonConvert.SerializeObject(info, Formatting.Indented));
+        return 0;
+    }
+}
---
+++ samples/Plugins/OldJsonPlugin/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using Contracts;
+global using CPlugin.Net;
+global using OldJsonPlugin;
+global using Newtonsoft.Json;
+global using System.Reflection;
