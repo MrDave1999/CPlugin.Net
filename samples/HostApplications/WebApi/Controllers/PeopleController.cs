0910b873c39665dd0d790bdd1259d430494d00fa
MrDave1999 <daveseva2010@hotmail.es>
2023-11-21 14:37:21 -0500

    docs: Add a controller as a sample

---
+++ samples/HostApplications/WebApi/Controllers/PeopleController.cs
@@ -0,0 +1,12 @@
+﻿namespace Example.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PeopleController
+{
+    private readonly List<Person> _persons;
+    public PeopleController(List<Person> persons) => _persons = persons;
+
+    [HttpGet]
+    public Result<List<Person>> GetAll() => Result.Success(_persons);
+}
