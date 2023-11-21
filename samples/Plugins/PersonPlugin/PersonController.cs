678eb6ae489245f5436b0fd9e0c1351fb3ff72c4
MrDave1999 <daveseva2010@hotmail.es>
2023-11-21 14:32:47 -0500

    docs: Add PersonPlugin project as a sample

---
+++ samples/Plugins/PersonPlugin/PersonController.cs
@@ -0,0 +1,10 @@
+﻿namespace Example.PersonPlugin;
+
+[ApiController]
+[Route("[controller]")]
+public class PersonController : ControllerBase
+{
+    [HttpGet] 
+    public Result<IEnumerable<Person>> GetAll(IPersonService service)
+        => service.GetAll();
+}
