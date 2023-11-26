0448322eae80898bc77c75824049fab8ab006765
MrDave1999 <daveseva2010@hotmail.es>
2023-11-26 13:00:35 -0500

    chore: Update global usings

--- tests/CPlugin.Net/GlobalUsings.cs
+++ tests/CPlugin.Net/GlobalUsings.cs
@@ -3,3 +3,6 @@ global using NUnit.Framework;
 global using FluentAssertions;
 global using DotEnv.Core;
 global using Microsoft.AspNetCore.Mvc.Testing;
+global using Newtonsoft.Json;
+
+global using TestProject.Contracts;
