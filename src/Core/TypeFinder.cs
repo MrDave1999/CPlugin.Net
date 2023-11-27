dccafd75b47b446456c699c8ae7ecf187e6f99db
MrDave1999 <daveseva2010@hotmail.es>
2023-11-27 17:04:58 -0500

    refactor: Add private method that encapsulates the logic for yield return
    If this change is not made, the internal method FindSubtypesOf will not execute the validations immediately, but when the .ToList method is called or used from a foreach.

--- src/Core/TypeFinder.cs
+++ src/Core/TypeFinder.cs
@@ -24,22 +24,37 @@ public static class TypeFinder
     /// An instance of type <see cref="IEnumerable{TSuperType}"/> that allows iterating over the instances
     /// that implement the contract specified by <typeparamref name="TSupertype"/>.
     /// <para>or</para>
-    /// Returns an empty enumerable if the <typeparamref name="TSupertype"/> does not have any subtype.
+    /// Returns an empty enumerable if the <typeparamref name="TSupertype"/> does not have any subtype, 
+    /// or if no assembly uses <see cref="PluginAttribute"/>.
     /// <para>This method never returns <c>null</c>.</para>
     /// </returns>
     /// <exception cref="ArgumentNullException">
     /// <c>assemblies</c> is <c>null</c>.
     /// </exception>
+    /// <exception cref="InvalidOperationException">
+    /// <c>TSupertype</c> is not an interface or an abstract class.
+    /// </exception>
     public static IEnumerable<TSupertype> FindSubtypesOf<TSupertype>() where TSupertype : class
         => FindSubtypesOf<TSupertype>(PluginLoader.Assemblies);
 
-    internal static IEnumerable<TSupertype> FindSubtypesOf<TSupertype>(
-        IEnumerable<Assembly> assemblies) where TSupertype : class
+    // This method is only to be used for testing.
+    // This way you don't have to depend on the plugin loader when testing.
+    internal static IEnumerable<TSupertype> FindSubtypesOf<TSupertype>(IEnumerable<Assembly> assemblies) 
+        where TSupertype : class
     {
         if (assemblies is null)
             throw new ArgumentNullException(nameof(assemblies));
 
-        foreach (Assembly assembly in assemblies) 
+        if(typeof(TSupertype).IsAbstract || typeof(TSupertype).IsInterface)
+            return GetSubtypesOf<TSupertype>(assemblies);
+
+        throw new InvalidOperationException($"'{typeof(TSupertype).FullName}' type must be abstract or an interface.");
+    }
+
+    private static IEnumerable<TSupertype> GetSubtypesOf<TSupertype>(IEnumerable<Assembly> assemblies) 
+        where TSupertype : class
+    {
+        foreach (Assembly assembly in assemblies)
         {
             var pluginAttributes = assembly.GetCustomAttributes<PluginAttribute>();
             foreach (PluginAttribute pluginAttribute in pluginAttributes)
