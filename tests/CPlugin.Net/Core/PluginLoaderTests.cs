ea8331f540f0a88845820a03db454678f4ec6675
MrDave1999 <daveseva2010@hotmail.es>
2023-11-28 13:23:52 -0500

    test: Add tests for PluginLoader

--- tests/CPlugin.Net/Core/PluginLoaderTests.cs
+++ tests/CPlugin.Net/Core/PluginLoaderTests.cs
@@ -31,12 +31,55 @@ public class PluginLoaderTests
         var versions = commands.Select(command =>
         {
             var json = command.Execute();
+            // Example: Newtonsoft.Json, Version=12.0.0.0, Culture=neutral, PublicKeyToken=null[0]
             var version = JsonConvert.DeserializeObject<Info>(json).Version;
+            // Example: Version=12.0.0.0
             return version.Split(",")[1].Trim();
         }).ToList();
 
         // Asserts
         commands.Should().HaveCount(expectedCommands);
         versions.Should().BeEquivalentTo(expectedVersions);
+    }
+
+    [Test]
+    public void Load_WhenArgumentIsNull_ShouldThrowArgumentNullException()
+    {
+        // Arrange
+        CPluginEnvConfiguration configuration = default;
+
+        // Act
+        Action act = () => PluginLoader.Load(configuration);
+
+        // Assert
+        act.Should()
+           .Throw<ArgumentNullException>()
+           .WithParameterName(nameof(configuration));
+    }
+
+    [Test]
+    public void Load_WhenMethodIsCalledMultipleTimes_ShouldNotLoadSamePluginsIntoMemory()
+    {
+        // Arrange
+        var value = "TestProject.HelloPlugin.dll";
+        Environment.SetEnvironmentVariable("PLUGINS", value);
+        var envConfiguration = new CPluginEnvConfiguration();
+        int expectedAssemblies = 1;
+
+        // Act
+        for (int i = 0; i < 10; i++) 
+        {
+            // This operation is idempotent.
+            PluginLoader.Load(envConfiguration);
+        }
+
+        // Assert
+        AppDomain
+            .CurrentDomain
+            .GetAssemblies()
+            .Where(assembly => assembly.GetName().Name == "TestProject.HelloPlugin")
+            .Count()
+            .Should()
+            .Be(expectedAssemblies);
     }
 }
