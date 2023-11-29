0d96cc8b216f7db244c91c78a7cdd2458f205e85
MrDave1999 <daveseva2010@hotmail.es>
2023-11-29 10:59:47 -0500

    test: Add PluginController type

---
+++ tests/TestProject.PluginHost/PluginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestProject.Contracts;
+
+namespace TestProject.PluginHost;
+
+[ApiController]
+[Route("[controller]")]
+public class PluginController : ControllerBase
+{
+    private readonly IEnumerable<IPluginStartup> _startups;
+    public PluginController(IEnumerable<IPluginStartup> startups) => _startups = startups;
+
+    [HttpGet]
+    public string[] GetInfo()
+        => _startups.Select(p => p.Name).ToArray();
+}
