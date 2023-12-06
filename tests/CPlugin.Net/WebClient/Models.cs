54df20012f973f92426d52e789fcb5a2ab54b057
MrDave1999 <daveseva2010@hotmail.es>
2023-12-06 14:40:46 -0500

    test: Create test models

---
+++ tests/CPlugin.Net/WebClient/Models.cs
@@ -0,0 +1,14 @@
+﻿namespace CPlugin.Net.Tests.WebClient;
+
+public class GetUserResponse
+{
+    public string Name { get; set; }
+    public string Password { get; set; }
+}
+
+public class GetEmployeeResponse
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Role { get; set; }
+}
