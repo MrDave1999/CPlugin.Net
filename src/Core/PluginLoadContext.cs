e9081ce63613cfe42d19ec7ab9acd14fc2bab63f
MrDave1999 <daveseva2010@hotmail.es>
2023-12-07 15:49:06 -0500

    fix-issue-27
    I don't really like this solution. I'm not sure how much it affects performance.

--- src/Core/PluginLoadContext.cs
+++ src/Core/PluginLoadContext.cs
@@ -6,6 +6,17 @@ namespace CPlugin.Net;
 
 internal class PluginLoadContext : AssemblyLoadContext
 {
+    private static readonly Dictionary<string, Version> s_defaultAssemblies;
+
+    static PluginLoadContext()
+    {
+        s_defaultAssemblies = Default
+            .Assemblies
+            .ToDictionary(
+                assembly => assembly.GetName().Name, 
+                assembly => assembly.GetName().Version);
+    }
+
     private readonly AssemblyDependencyResolver _resolver;
 
     public PluginLoadContext(string pluginPath)
@@ -15,6 +26,19 @@ internal class PluginLoadContext : AssemblyLoadContext
 
     protected override Assembly Load(AssemblyName assemblyName)
     {
+        // This is to avoid loading the CPlugin.Net.Attributes.dll assembly in the custom context.
+        // This should be loaded in the default context.
+        if (assemblyName.Name == "CPlugin.Net.Attributes")
+            return default;
+
+        // This is to avoid loading the same version of an assembly.
+        // If the assembly exists, it means that the default load context already has it.
+        bool assemblyExists = s_defaultAssemblies.TryGetValue(assemblyName.Name, out Version version);
+        if (assemblyExists && assemblyName.Version.ToString() == version.ToString())
+        {
+            return default;
+        }
+
         var assemblyPath = _resolver.ResolveAssemblyToPath(assemblyName);
         return assemblyPath is null ? default : LoadFromAssemblyPath(assemblyPath);
     }
